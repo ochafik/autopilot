fc913e1f16c3a30eeb91a0ce8a10bc6f5702c930
Olivier Chafik <olivier.chafik@gmail.com>
2014-04-20 20:43:44 +0200

    mac socks script

---
+++ mac_socks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+readonly STATE="$1"
+readonly HOST="$2"
+readonly SOCKS_PORT="${SOCKS_PORT:-8080}"
+readonly SSH_PORT="${SSH_PORT:-1022}"
+readonly HOST_USER="${HOST_USER:-pi}"
+readonly INTERFACE=${INTERFACE:-Wi-Fi}
+
+case $STATE in
+    off)
+        sudo networksetup -setsocksfirewallproxystate $INTERFACE off
+        ;;
+    on)
+        if [[ -z "$HOST" ]]; then
+            echo "Please specify a host."
+            exit 1
+        fi
+        ssh $HOST_USER@$HOST:$SSH_PORT "sudo /root/autopilot/allow_current_ssh_client.sh"
+
+        sudo networksetup -setsocksfirewallproxy $INTERFACE $HOST $SOCKS_PORT off
+        sudo networksetup -setsocksfirewallproxystate $INTERFACE on
+        ;;
+    *)
+        echo "Unknown state: '$STATE'"
+        ;;
+esac
+
